ee6ea77df37171366a8b99d5c14ceb905e6ecc11
Bre <bre@breannabaker.com>
2019-04-28 13:14:51 -0400

    2019.04.28 javascript commit 2

---
+++ Drawing1c_Cow.js
@@ -0,0 +1,22 @@
+// Javascript 1c Drawing
+// Cow 
+
+triangle(80, 40, 70, 45, 73, 20); // left horn
+triangle(120, 40, 130, 45, 127, 20); //right horn
+ellipse(50, 70, 90, 40); //left ear
+ellipse(50, 70, 70, 20); // left ear inner
+ellipse(150, 70, 90, 40); //right ear
+ellipse(150, 70, 70, 20); // right ear inner
+ellipse(100, 100, 140, 130); //head
+ellipse(70, 95, 30, 60); // left eye
+ellipse(70, 100, 15, 40); // left pupil
+ellipse(130, 95, 30, 60); //right eye
+ellipse(130, 100, 15, 40); // left eye
+rect(20, 110, 160, 60); //snout
+ellipse(100, 140, 100, 20); //mouth
+line(50, 140, 150, 140); // teeth
+
+// line( start point, end point);
+
+// result: https://i.imgur.com/VOhz1he.png
+// lesson: https://www.khanacademy.org/computing/computer-programming/programming/drawing-basics/pt/drawing-more-shapes-with-code
