faa423a33ac44b0821be4e0edd47acdec4105396
Bre <bre@breannabaker.com>
2019-05-04 00:46:13 -0400

    2019.05.04 midnight commit 1

---
+++ Intro2JS_DrawingAnimation/6_Resizing.with.Variables_5.03/Resizing6a_Math.Expressions.js
@@ -0,0 +1,20 @@
+/* mathematical operators are used to calculate numbers + create expressions. 
+    + addition 
+    - subtraction 
+    * multiplication 
+    / division 
+    % remainder
+
+computer follows "order of operations" to compute results the same way each time. 
+"PEMDAS" 
+    Parenteses 
+    Exponents 
+    Multiplication 
+    Division 
+    Addition 
+    Subtraction
+
+Parenthese are only needed if an expression needs to be 
+evaluated differently according to the order of operations. 
+
+*/
