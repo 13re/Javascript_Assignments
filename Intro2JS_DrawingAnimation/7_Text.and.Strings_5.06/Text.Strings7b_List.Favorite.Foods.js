823dd3c3755dd672b1a98400e734b027ccbfb7f3
Bre <bre@breannabaker.com>
2019-05-07 23:42:44 -0400

    2019.05.07 commit night1

---
+++ Intro2JS_DrawingAnimation/7_Text.and.Strings_5.06/Text.Strings7b_List.Favorite.Foods.js
@@ -0,0 +1,17 @@
+// List 3 of Favorite Foods
+
+background(204, 209, 217);
+
+fill(135, 0, 0);
+textSize(24);
+text("My Favortie Foods", 50, 50); // 3 arguments: text, x + y position. 
+
+fill(255, 0, 0);
+textSize(20);
+
+text("Ice Cream", 75, 100);
+text("Saag Aloo", 75, 150);
+text("Bananas", 75, 200);
+
+// result: https://i.imgur.com/YlxvcLb.png
+// lesson: https://www.khanacademy.org/computing/computer-programming/programming/text-basics/pc/challenge-my-favorite-foods
