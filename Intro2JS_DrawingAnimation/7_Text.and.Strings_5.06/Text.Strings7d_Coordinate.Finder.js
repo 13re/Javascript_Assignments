68a5213cbf66ceca90917ee4813d17d32064bf4a
Bre <bre@breannabaker.com>
2019-05-08 10:09:39 -0400

    2019.05.08 commit 2

---
+++ Intro2JS_DrawingAnimation/7_Text.and.Strings_5.06/Text.Strings7d_Coordinate.Finder.js
@@ -0,0 +1,14 @@
+// Coordinate Finder
+
+fill(30, 148, 67);
+
+draw = function() {
+    background(255, 255, 255);
+    ellipse(mouseX, mouseY, 12, 12); 
+    var mouseLocation = (mouseX + "," + mouseY);
+    text(mouseLocation, mouseX + 5, mouseY - 10);
+    
+};
+
+// result: https://youtu.be/gqRsxL7P1jY
+// lesson: https://www.khanacademy.org/computing/computer-programming/programming/text-basics/pc/challenge-mouse-tracker
