7d2437a0c36a5dacd618e1eb32d1c04e7e82777f
Bre <bre@breannabaker.com>
2019-05-10 09:21:12 -0500

    2019.05.10 commit 9am

--- Intro2JS_DrawingAnimation/8_Functions_5.09/Functions8b_Parameters.js
+++ Intro2JS_DrawingAnimation/8_Functions_5.09/Functions8b_Parameters.js
@@ -5,5 +5,7 @@
 To specify the location of the code every time we call the function, 
 we must specify parameters in function definition + function call. 
 
-To tell the function that we're passing parameters, we add
+To tell the function that we're passing parameters, 
+we add parameter names like shapeX and shapeY inside the parenthesis. 
+Now the function will use the parameters we specify when calling the new function. 
 */
---
+++ Intro2JS_DrawingAnimation/8_Functions_5.09/Functions8c_Rain.Windshield.js
@@ -0,0 +1,34 @@
+// Rain on windshield
+
+background(107, 111, 179);
+noStroke();
+// clouds
+
+// top darkest cloud
+fill(134, 143, 156);
+ellipse(200, 25, 500, 100); 
+// mid grey clouds
+fill(176, 182, 191);
+ellipse(250, 100, 250, 75); 
+ellipse(260, 75, 200, 75);
+ellipse(75, 50, 200, 75);
+// light grey clouds
+fill(208, 212, 219);
+ellipse(125, 75, 200, 50);
+ellipse(100, 50, 100, 50);
+ellipse(325, 50, 200, 75); 
+
+
+var rainDrop = function(){
+    var rainY = random(1, 400);
+    var rainX = random(1, 400);
+    fill(211, 234, 235);
+    ellipse(rainX, rainY, 5, 10);
+};
+
+draw = function() {
+    rainDrop();
+};
+    
+// result: https://youtu.be/bmiaQVdEgaM
+// lesson: https://www.khanacademy.org/computing/computer-programming/programming/functions/pt/function-parameters
