e253217b85ab9c307dfb4a349ba89b9de0801903
Bre <bre@breannabaker.com>
2019-05-18 23:53:56 -0700

    2019.05.18 commit 11pm

---
+++ Intro2JS_DrawingAnimation/8_Functions_5.09/Functions8h_Review.Notes.js
@@ -0,0 +1,23 @@
+/* NOTES
+Review: Functions
+
+Function 
+    // a way to re-execute blocks of code when writing programs, 
+    without having to re-write the block of code entirely. 
+    // Group code together, give it a name to call later.
+    // Functions must first be declared + given a name (same way any variable is created),
+    then follow it w/ a function definition.
+        var sayHi = function(){
+            anyCode
+        };
+    Any code could go inside that statement - one statement, multiple statements...
+    
+    If a function is only declared, nothing will happen. We must "call" the function
+    in order for the program to execute the code inside the function. 
+    To call a function, write: functionName();
+
+    We can call functions whenever we want + however many times we want. 
+
+
+
+*/
