bec27710398587b575740782b81a4d290fde61e1
Bre <bre@breannabaker.com>
2019-05-21 07:08:47 -0700

    2019.05.21 commit 7am2

---
+++ Intro2JS_DrawingAnimation/9_If.Statements_5.20/If.Statements9f_Color.Change.Button.js
@@ -0,0 +1,18 @@
+// Color Change Button
+
+draw = function() {
+    fill(0, 255, 68); // start color
+    if (mouseIsPressed && mouseY <= 200){
+        fill(255, 0, 0);
+        
+    }
+    rect(0, 0, 400, 200);  // the button
+
+    // The button text
+    fill(0, 0, 0);
+    textSize(25);
+    text("Click to turn Red", 100, 115);
+};
+
+// result: https://youtu.be/A8hg7nLwVHQ
+// lesson: https://www.khanacademy.org/computing/computer-programming/programming/logic-if-statements/pc/challenge-your-first-button
