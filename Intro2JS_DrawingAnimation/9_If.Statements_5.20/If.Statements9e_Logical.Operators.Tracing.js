148501b84d2c3ae50dd5cade638ea56bcf231846
Bre <bre@breannabaker.com>
2019-05-21 06:58:17 -0700

    2019.05.21 commit 7am

---
+++ Intro2JS_DrawingAnimation/9_If.Statements_5.20/If.Statements9e_Logical.Operators.Tracing.js
@@ -0,0 +1,38 @@
+// Logical Operators - Tracing
+// && and
+// || or
+
+// This program draws green when correctly tracing the letter "L", 
+// and draws red when drawing outside the lines. 
+
+background(232, 219, 208);
+
+// L outline
+noStroke();
+fill(255, 255, 255);
+rect(130, 250, 150, 30); // horizontal line
+rect(130, 80, 30, 170); // vertical line
+
+var notWithinLines = 255;
+var withinLines = 0;
+
+draw = function() {
+    
+    // draws green within the lines & red outside the lines
+    if ((mouseY > 70 && mouseY < 290 && mouseX > 120 && mouseX < 160) || (mouseX > 120 && mouseX < 290 && mouseY > 240 && mouseY < 290)){
+        notWithinLines = 0;
+        withinLines = 255; // COLOR: green 
+        }
+    else{
+        notWithinLines = 255; // COLOR: red
+        withinLines = 0;
+    }
+    // draws when mouse is pressed
+    if (mouseIsPressed){
+        fill(notWithinLines, withinLines, 0);
+        ellipse(mouseX, mouseY, 30, 30);
+    }
+};
+
+// result: https://youtu.be/zv1ZkkgY1-o
+// lesson: https://www.khanacademy.org/computing/computer-programming/programming/logic-if-statements/pt/logical-operators
