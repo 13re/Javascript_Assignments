8c3386b1e810b69f36e016959a52717beac1b421
Bre <bre@breannabaker.com>
2019-05-22 11:18:30 -0700

    2019.05.22 commit 11am2

---
+++ Intro2JS_DrawingAnimation/9_If.Statements_5.20/If.Statments9k_Review.js
@@ -0,0 +1,34 @@
+/* Review Notes
+
+if statement
+    tells the program to execute a block of code only if a condition is true
+else statement
+    executes some block of code in the case that the if condition is NOT true
+conditionally 
+    if this thing is true, then do X, but if this other thing is true, then do Y
+    we can do things "conditionally" in our programs using "if statements" & "if/else statements" combined w/ "conditional expressions"
+conditional expression / conditionals
+    an expression that evaluates to either true or false
+    uses comparison operators (comparisons) to evaluate whether true or false
+boolean 
+    a value that is either true or false (rather than a number or string)
+    //
+    var x = 0;
+    text(x > 0, 200, 200); // Displays "true"
+    //
+    var isPositive = x > 0; // isPositive is storing a bloolean value
+    text(isPositive, 200, 200); // Displays "true" 
+comparison operators / comparisons
+    === // Strict equality
+    !== // Strict inequality
+    > // Greater than
+    >= // Greater than or equal
+    < // Less than 
+    <= // Less than or equal
+assignent operator
+    = // changes the value of the variable
+logical operators
+    && // and // requires 2 conditions are both true
+    || // or // only requires one condition is true
+
+*/
