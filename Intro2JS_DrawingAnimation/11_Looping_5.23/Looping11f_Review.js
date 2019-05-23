3201b9f6a54ab6ada115854acfd4e54ff1addf5f
Bre <bre@breannabaker.com>
2019-05-23 12:33:57 -0700

    2019.05.23 commit 12pm noon2

---
+++ Intro2JS_DrawingAnimation/11_Looping_5.23/Looping11f_Review.js
@@ -0,0 +1,21 @@
+// Looping - Review
+
+/* NOTES
+
+while loop
+    a way to repeat code until some condition is false
+for loop
+    = similar to a while loop, but w/ more specialized syntax
+    Programmers invented after repetition of doing the same 3 things:
+        creating loop counter variables
+        incrementing them by some amount
+        checking that they're less than a value
+infinite loop
+    when the condition iside the parentesis is always true within a loop
+    Loops forever - program never knows when to stop. 
+
+nested loops
+    nesting a loop inside another loop
+    Tells program "do this thing X many times, and for each time you do that, also do this other thing Y many time."
+    ex: grids    
+*/
