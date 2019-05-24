6329d2bcd5caf1f2e90411c9456e5f9027ab09ef
Bre <bre@breannabaker.com>
2019-05-23 20:07:11 -0700

    2019.05.23 commit 8pm

---
+++ Intro2JS_DrawingAnimation/12_Writing.Clean.Code_5.23/Clean.Code12a_Readable.Code.js
@@ -0,0 +1,16 @@
+// Readable Code
+
+// shouldn't have to read multiple times to understand
+// use proper punctuation/spacing like in English
+
+/*
+Ways to make code more readable:
+    indenting
+        when a child
+        when inside a function, loop, or if block
+    spacing (whitespace)
+        between function arguments
+        on each side of equals
+        around operators
+        before curly brackets
+*/
---
+++ Intro2JS_DrawingAnimation/12_Writing.Clean.Code_5.23/Clean.Code12b_Claryfing.with.Comments.js
@@ -0,0 +1,21 @@
+// Clarifying with Comments (notes)
+
+/* Write general description of program at the top each time */
+
+/*
+comments are for describing our code to humans, 
+because code is not our native language. 
+
+helps us to better understand & modify our own code
+
+helps others learn from your code
+helps others fix bugs & add features to your code
+helps yourself to understand your own code in future
+
+Further things to research
+    "JavaScript coding style conventions"
+    "JavaScript naming conventions"
+
+*/
+
+/* multi-line comment */
