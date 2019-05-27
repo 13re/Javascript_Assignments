f61de00df462dd10e77bcf3eca9cec9c76e7204f
Bre <bre@breannabaker.com>
2019-05-27 11:32:25 -0400

    2019.05.27 commit 11am Arrays Review

--- Intro2JS_DrawingAnimation/13_Arrays_5.23/Arrays13g_Review.js
+++ Intro2JS_DrawingAnimation/13_Arrays_5.23/Arrays13g_Review.js
@@ -7,6 +7,15 @@ Array
     declare a variable, but surround our list of values w/ square brackets & seperate each value with a comma.
     var xPositions = [22, 75, 40];
     var myFriends = ["Billy", "Bob", "Joe", "Junior"];
+    array sample: [element, element, element, element];
+    *can use a for loop to iterate through each element in an array & do something to each element inside the loop. Start from index 0, go until the end of the array is reached by adding 1 to the index each time. 
+        for (var i = 0; i < myFriends.length; i++) {
+            text(myFriends[i], 200, (i+1)*100);
+        }
+    *change the value in an array like this:
+        myFriends[1] = "TheErrorBuddy";
+    *add new values to an array using the push() method:
+        myFriends.push("Hopper");
 arrayName.length
     the length property will tell us the current length of the array.
     text(myFriends.length, 200, 200); // Displays "4"
